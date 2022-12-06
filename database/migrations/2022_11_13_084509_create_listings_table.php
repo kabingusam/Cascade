464667b0cc4e7106880aef89fb9151307b9bf67c
kabingusam <kabingusammy@gmail.com>
2022-12-06 19:13:06 +0300

    varible update in migrations

--- database/migrations/2022_11_13_084509_create_listings_table.php
+++ database/migrations/2022_11_13_084509_create_listings_table.php
@@ -14,6 +14,7 @@ class CreateListingsTable extends Migration
     public function up()
     {
         Schema::create('listings', function (Blueprint $table) {
+            
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
