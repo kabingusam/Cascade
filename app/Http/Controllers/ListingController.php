ed1eafd6d79358e25df4551910b5b913a3372566
kabingusam <kabingusammy@gmail.com>
2022-12-06 19:11:50 +0300

    controller function update

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -27,7 +27,6 @@ class ListingController extends Controller
     public function create(){
         return view('listings.create');
     }
-
     //store listing data
     public function store(Request $request){
         $formFields = $request->validate([
